b220aa96a4f533c2626e2f9a29d7e0fc98f61095
theoinisan <theo@swapcard.com>
2022-09-23 00:22:13 +0200

    fix: force styles

--- src/layout.tsx
+++ src/layout.tsx
@@ -18,45 +18,45 @@ const Reset = createGlobalStyle`
     }
 
     .style__UIBox-sc-0-0 {
-      border-radius: 0px;
-      border-color: #f2f2f2;
+      border-radius: 0px !important;
+      border-color: #f2f2f2 !important;
     }
 
     .list__Title-sc-0-0 {
-      background: none;
-      padding: 0;
-      font-weight: 900
+      background: none !important;
+      padding: 0 !important;
+      font-weight: 900 !important;
     }
 
     .grid__Name-sc-0-4 {
-      font-size: 20px;
-      padding: 30px 0 10px 0;
+      font-size: 20px !important;
+      padding: 30px 0 10px 0 !important;
     }
 
     .container__Grid-sc-0-1 {
-      grid-gap: 30px;
-      grid-template-columns: repeat(auto-fill,minmax(250px,1fr));
+      grid-gap: 30px !important;
+      grid-template-columns: repeat(auto-fill,minmax(250px,1fr)) !important;
 
       .style__UIBox-sc-0-0 {
-        text-align: center;
+        text-align: center !important;
       }
     }
 
     .wrapper__Wrapper-sc-0-0 {
-      max-width: 1400px;
+      max-width: 1400px !important;
     }
 
     .item__ItemHeader-sc-0-1 {
-      min-height: 50px;
+      min-height: 50px !important;
 
       .label__Label-sc-0-0 {
-        font-size: 16px;
-        color: #3e3f65;
+        font-size: 16px !important;
+        color: #3e3f65 !important;
       }
     }
 
     .wrapper__Wrapper-sc-0-0 {
-      grid-gap: 30px;
+      grid-gap: 30px !important;
     }
 
     .aside__Aside-sc-0-0 {
@@ -65,7 +65,7 @@ const Reset = createGlobalStyle`
       }
 
       .desktop__Header-sc-0-0, .hnprzT, .divider__SimpleDivider-sc-0-0 {
-        display: none;
+        display: none !important;
       }
     }
 
@@ -74,24 +74,24 @@ const Reset = createGlobalStyle`
     }
 
     .grid__Booth-sc-0-6 {
-      display: none;
+      display: none !important;
     }
 
     .style__Wrapper-sc-0-0.MwmHP  {
-      border-radius: 0;
-      margin-bottom: 15px;
+      border-radius: 0 !important;
+      margin-bottom: 15px !important;
 
       .custom__Left-sc-0-1 {
-        display: none;
+        display: none !important;
       }
 
       .input__Input-sc-0-0 {
-        padding-left: 15px;
+        padding-left: 15px !important;
       }
     }
 
     .style__CheckMarkContainer-sc-0-1 {
-      border-radius: 0;
+      border-radius: 0 !important;
     }
 `;
 
